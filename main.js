d4f84e42decd22445cdf46163057392beee97a5b
lenaraom <choucomo@gmail.com>
2022-01-15 21:04:33 +0100

    php files

--- main.js
+++ main.js
@@ -8,3 +8,24 @@ window.onscroll = function showHeader() {
         header.classList.remove('navbar_fixed');
     }
 }
+
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Danke!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
